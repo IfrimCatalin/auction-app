299c261a77468c902a38683e61cbde3852be8016
Ifrim Catalin <ifrimcatalin1959@gmail.com>
2026-05-08 16:11:13 +0300

    Initial Next.js project

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold mb-4">
+          Auction Marketplace
+        </h1>
+
+        <p className="text-gray-400 text-xl">
+          Built by Catalin 🚀
+        </p>
+      </div>
+    </main>
+  );
+}
